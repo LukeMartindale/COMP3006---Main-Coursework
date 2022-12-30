ce5d0f0518b1f86edaf7c2748470bc35b3b12100
LukeMartindale <luke.martindale@hotmail.co.uk>
2022-12-30 17:32:11 +0000

    Message notification banner show

--- static/javascript/app/friends-page.js
+++ static/javascript/app/friends-page.js
@@ -13,6 +13,30 @@ $(function(){
                 let val = parseInt($("#" + data.direct_requests[i].senderId + "-alert-counter").text())
                 val++
                 $("#" + data.direct_requests[i].senderId + "-alert-counter").text(val)
+
+                if($("#friend-alert-counter").hasClass("no-alerts")){
+                    $("#friend-alert-counter").removeClass("no-alerts")
+                }
+                let val_2 = parseInt($("#friend-alert-counter").text())
+                val_2++
+                $("#friend-alert-counter").text(val_2)
+            }
+        }
+    });
+
+    $.ajax({
+        url: "http://localhost:9000/app/notifications/group/unread/",
+        type: 'GET',
+        datatype: 'json',
+        success: function(data){
+            console.log(data)
+            for(let i=0; i< data.group_requests.length; i++){
+                if($("#group-alert-counter").hasClass("no-alerts")){
+                    $("#group-alert-counter").removeClass("no-alerts")
+                }
+                let val = parseInt($("#group-alert-counter").text())
+                val++
+                $("#group-alert-counter").text(val)
             }
         }
     });
--- static/javascript/app/notification-page.js
+++ static/javascript/app/notification-page.js
@@ -7,20 +7,12 @@ $(function(){
         success: function(data){
             console.log(data)
             for(let i=0; i< data.group_requests.length; i++){
-                if($("#" + data.group_requests[i].senderId + "-alert-counter").hasClass("no-alerts")){
-                    $("#" + data.group_requests[i].senderId + "-alert-counter").removeClass("no-alerts")
-                }
-                let val = parseInt($("#" + data.group_requests[i].senderId + "-alert-counter").text())
-                val++
-                $("#" + data.group_requests[i].senderId + "-alert-counter").text(val)
-
                 if($("#group-alert-counter").hasClass("no-alerts")){
                     $("#group-alert-counter").removeClass("no-alerts")
                 }
-                let val_2 = parseInt($("#group-alert-counter").text())
-                val_2++
-                $("#group-alert-counter").text(val_2)
-
+                let val = parseInt($("#group-alert-counter").text())
+                val++
+                $("#group-alert-counter").text(val)
             }
         }
     });
