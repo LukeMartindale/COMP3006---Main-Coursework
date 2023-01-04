79afc4f42c2d44dd3bac4f835ad2b901580a6b62
LukeMartindale <luke.martindale@hotmail.co.uk>
2023-01-04 16:55:58 +0000

    Finished Group Tests and Written Tests for dm

--- tests/chat.tests.js
+++ tests/chat.tests.js
@@ -8,6 +8,8 @@ let mongoose = require("mongoose");
 let User = require("../database/models/model.user").User;
 let Group = require("../database/models/model.group").Group;
 let Message = require("../database/models/model.message").Message;
+let Request = require("../database/models/model.request").Request;
+let DirectMessage = require("../database/models/model.direct-message").DirectMessage;
 
 suite("Chat Group Suite", function(){
 
@@ -17,7 +19,6 @@ suite("Chat Group Suite", function(){
         setTimeout(function(){
             done()
         }, 500)
-
     })
 
     suiteSetup(function(done){
@@ -45,7 +46,6 @@ suite("Chat Group Suite", function(){
     suiteTeardown(function(done){
         mongoose.connection.close()
         this.server.close()
-        // this.sandbox.restore()
 
         setTimeout(function(){
             done()
@@ -82,6 +82,18 @@ suite("Chat Group Suite", function(){
 
     teardown(function(done){
         Group.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        Message.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        Request.deleteMany({}, (error) => {
             done()
         })
     })
@@ -257,4 +269,199 @@ suite("Chat Group Suite", function(){
         });
     });
 
+    test("Test can leave a group you are a member of", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(async function(error, response) {
+            let group = await Group.findOne({"group_name": "existing_group"})
+            agent.post("/chat/group/leave/").send({"groupId": group._id.toString()}).end(function(error, response){
+                chai.assert.equal(response.status, 200, "Status code is not correct!")
+                chai.assert.equal(JSON.parse(response.text).message, "Group left!", "Group was not left")
+                done()
+            });
+        });
+    });
+
+    test("Test can delete a group you are a member of", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(async function(error, response) {
+            let group = await Group.findOne({"group_name": "existing_group"})
+            agent.post("/chat/group/delete/").send({"groupId": group._id.toString()}).end(async function(error, response){
+                group = await Group.findOne({"group_name": "existing_group"})
+                chai.assert.equal(response.status, 200, "Status code is not correct!")
+                chai.assert.equal(JSON.parse(response.text).message, "Group deleted!", "Group was not left")
+                chai.assert.equal(group, null, "Group was not deleted")
+                done()
+            });
+        });
+    });
+
 });
+
+suite("Chat Direct Message Suite", function(){
+
+    suiteSetup(function(done){
+        this.server = require("../server/server"); 
+
+        setTimeout(function(){
+            done()
+        }, 500)
+    })
+
+    suiteSetup(function(done){
+
+        //create credentials for an existing user on the system
+        this.existing_user = {
+            "username": "existing-user",
+            "password": "existing-password",
+        }
+
+        this.new_user = {
+            "username": "new-user",
+            "password": "new-password",
+        }
+
+        this.other_user = {
+            "username": "other-user",
+            "password": "other-password"
+        }
+
+        //Connect to test database
+        let url = `mongodb+srv://TheMartindale:dxWrO4fnEic4ay8A@cluster0.rh5pgvb.mongodb.net/slanttestdb?retryWrites=true&w=majority`;
+        mongoose.connect(url)
+        .then(function(){
+            done()
+        })
+
+    })
+
+    setup(function(done){
+        chai.request(this.server).post("/auth/signup/").send(this.existing_user).end(function(error, response){
+            done()
+        })
+    })
+
+    setup(function(done){
+        chai.request(this.server).post("/auth/signup/").send(this.new_user).end(function(error, response){
+            done()
+        })
+    })
+
+    setup(function(done){
+        chai.request(this.server).post("/auth/signup/").send(this.other_user).end(function(error, response){
+            done()
+        })
+    })
+
+    setup(async function(){
+        let existing_user = await User.findOne({"username": this.existing_user.username})
+        let new_user = await User.findOne({"username": this.new_user.username})
+
+        let directmessage = new DirectMessage({
+            "group_members": [existing_user._id.toString(), new_user._id.toString()]
+        })
+        await directmessage.save()
+
+        dm = await DirectMessage.findOne()
+        existing_user.friends.push({
+            "friend": new_user._id,
+            "dm_id": dm._id
+        })
+        new_user.friends.push({
+            "friend": existing_user._id,
+            "dm_id": dm._id
+        })
+
+        await existing_user.save()
+        await new_user.save()
+
+    })
+
+    teardown(function(done){
+        User.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        Group.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        Message.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        Request.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    teardown(function(done){
+        DirectMessage.deleteMany({}, (error) => {
+            done()
+        })
+    })
+
+    test("Test can get Specific direct message page", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(async function(error, response) {
+            let directmessage = await DirectMessage.findOne()
+            agent.get("/chat/direct/" + directmessage._id.toString()).end(function(error, response){
+                chai.assert.equal(response.status, 200, "Status code is not correct")
+                done()
+            })
+        });
+    })
+
+    test("Test cannot get Specific direct message page not a member of", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.other_user).end(async function(error, response) {
+            let directmessage = await DirectMessage.findOne()
+            agent.get("/chat/direct/" + directmessage._id.toString()).end(function(error, response){
+                chai.assert.equal(response.status, 400, "Status code is not correct")
+                chai.assert.equal(JSON.parse(response.text).message, "User is not in this dm!", "Message was not sent!")
+                done()
+            })
+        });
+    })
+
+    test("Test can send Direct Message Text Message", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(async function(error, response) {
+            let directmessage = await DirectMessage.findOne()
+            agent.post("/chat/direct/").send({"text": "test-message", "type": "text", "dm_id": directmessage._id.toString(), "senderUsername": "existing-user"}).end(function(error, response){
+                chai.assert.equal(response.status, 200, "Status code is not correct")
+                chai.assert.equal(JSON.parse(response.text).message, "Message was sent!", "Message was not sent!")
+                done()
+            })
+        });
+    })
+
+    test("Test can send Direct Message Image Message", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(async function(error, response) {
+            let directmessage = await DirectMessage.findOne()
+            agent.post("/chat/direct/").send({"text": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png", "type": "image", "dm_id": directmessage._id.toString(), "senderUsername": "existing-user"}).end(function(error, response){
+                chai.assert.equal(response.status, 200, "Status code is not correct")
+                chai.assert.equal(JSON.parse(response.text).message, "Message was sent!", "Message was not sent!")
+                done()
+            })
+        });
+    })
+
+    test("Test cannot send empty Direct Message Message", function(done){
+        let agent = chai.request.agent(this.server)
+        agent.post("/auth/login/").send(this.existing_user).end(function(error, response) {
+            agent.post("/chat/direct/").send({"text": ""}).end(function(error, response){
+                chai.assert.equal(response.status, 400, "Status code is not correct")
+                chai.assert.equal(JSON.parse(response.text).message, "Cannot send an empty message!", "Message was not empty!")
+                done()
+            })
+        });
+    })
+
+})
